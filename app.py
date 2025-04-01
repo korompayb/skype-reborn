f7897fcfd6b6cee3e900e2e2b0b892362c6a90f5
Rendszergazda <rendszergazda@blathy.info>
2025-04-01 10:13:17 +0200

    hotfix

--- app.py
+++ app.py
@@ -151,8 +151,8 @@ def logout():
         session.pop("username", None)
     return redirect("/")
 
-""" if __name__ == "__main__":
-    app.run(port=5000, host='0.0.0.0', threaded=True) """
-
 if __name__ == "__main__":
-    app.run(debug=True , port=5000)
+    app.run( debug=True,port=5000, host='0.0.0.0', threaded=True)
+
+""" if __name__ == "__main__":
+    app.run(debug=True , port=5000) """
